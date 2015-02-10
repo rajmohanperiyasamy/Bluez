b5e0f6c10cdccacefeb8e3562a59f0358634a4be
Raj <raj@raj-desktop.(none)>
2015-02-11 02:20:33 +0530

    email third commit

--- bluez/settings.py
+++ bluez/settings.py
@@ -36,8 +36,8 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rajmohanjr@gmail.com'
-EMAIL_HOST_PASSWORD = 'doublespring1'
+EMAIL_HOST_USER = 'chandruit15@gmail.com'
+EMAIL_HOST_PASSWORD = 'computerp'
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = '<address>'
 
